0add5886afae11ac791d0c20e6f51d0082672eb3
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2023-07-11 00:09:16 -0400

    Completed navbar and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { HomeComponent } from './home/home.component';
+import { BoardComponent } from './board/board.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CreatePostComponent,
+    HomeComponent,
+    BoardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  title = "Get Help Or Help Others!"
+  home_title: string = "In a world that often feels divided and disconnected, the power of community and helping one another shines brighter than ever. It's in the moments when we extend a helping hand, lend an empathetic ear, or offer a kind word that we truly come alive as human beings."
+  second_title: string = " We have the capacity to lift each other up, to share burdens, and to spread love and compassion. When we choose to get help or help others, we create a ripple effect of positivity that can transform lives, heal wounds, and build bridges."
+  third_title: string = "Together, let us embrace the beauty of community, recognizing that our collective strength lies in our ability to support and uplift one another."
+  
+}
