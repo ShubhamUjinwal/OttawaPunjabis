66df553ba43b0fa7320c715da09b4039fe4831c4
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2023-07-16 00:52:45 -0400

    Sort list of rideshare items and added shadow on card title

--- src/app/board/rideshare-list/rideshare-list.component.ts
+++ src/app/board/rideshare-list/rideshare-list.component.ts
@@ -8,16 +8,22 @@ import { Rideshare } from './rideshare.model';
 })
 export class RideshareListComponent {
   rideshare_posts: Rideshare[] = [
-    new Rideshare('Offering', 'Ottawa', 'Toronto', '14 July 2023', 50, 'shubhamujinwal'),
+    new Rideshare('Offering', 'Ottawa', 'Toronto', '25 July 2023', 50, 'shubhamujinwal'),
     new Rideshare('Looking For', 'Brampton', 'Montreal', '16 July 2023', 40, 'abcd_1234'),
-    new Rideshare('Looking For', 'Missussauga', 'Oshawa', '18 July 2023', 60, 'abcd_1234'),
+    new Rideshare('Looking For', 'Mississauga', 'Oshawa', '18 July 2023', 60, 'abcd_1234'),
     new Rideshare('Offering', 'Hamilton', 'Ottawa', '20 July 2023', 30, 'abcd_1234'),
     new Rideshare('Looking For', 'Niagara Falls', 'Ottawa', '20 July 2023', 30, 'abcd_1234'),
     new Rideshare('Looking For', 'Niagara Falls', 'Ottawa', '20 July 2023', 30, 'abcd_1234'),
     new Rideshare('Looking For', 'Niagara Falls', 'Ottawa', '20 July 2023', 30, 'abcd_1234')
   ];
 
-  constructor() {}
+  constructor() {
+    this.rideshare_posts.sort((a, b) => {
+      if (a.available_on < b.available_on) return -1;
+      else if (a.available_on > b.available_on) return 1;
+      else return 0
+    });
+  }
 
   ngOnInit() {}
 }
