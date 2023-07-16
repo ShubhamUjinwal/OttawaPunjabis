0eb97753aa3b58bb5fa9968264bc42279309e32d
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2023-07-16 00:09:50 -0400

    Random color gradient selection for rideshare cards.

--- src/app/board/rideshare-list/rideshare-item/rideshare-item.component.ts
+++ src/app/board/rideshare-list/rideshare-item/rideshare-item.component.ts
@@ -9,10 +9,30 @@ import { Rideshare } from '../rideshare.model';
 export class RideshareItemComponent implements OnInit{
  
   @Input() rideshare_post!: Rideshare;
+  @Input() color_index!: number;
+
+  bg_colors = [
+    ['#ffafbd', '#ffc3a0'],
+    ['#2193b0', '#6dd5ed'],
+    ['#cc2b5e', '#753a88'],
+    ['#ee9ca7', '#ffdde1'],
+    ['#42275a', '#734b6d'],
+    ['#bdc3c7', '#2c3e50'],
+    ['#de6262', '#ffb88c'],
+    ['#06beb6', '#48b1bf'],
+    ['#eb3349', '#f45c43'],
+    ['#dd5e89', '#f7bb97']
+  ]
+  color = '#dd5e89'
 
   constructor() {}
 
   ngOnInit(): void {
-    
+    console.log(this.getRandomColor());
+  }
+
+  getRandomColor() {
+    var getRandomColorIndex =  this.bg_colors[this.color_index % this.bg_colors.length]
+    return {'background': "linear-gradient(to right,"+ getRandomColorIndex[0]+","+ getRandomColorIndex[1] +")"}
   }
 }
--- src/app/board/rideshare-list/rideshare-list.component.ts
+++ src/app/board/rideshare-list/rideshare-list.component.ts
@@ -11,7 +11,10 @@ export class RideshareListComponent {
     new Rideshare('Offering', 'Ottawa', 'Toronto', '14 July 2023', 50, 'shubhamujinwal'),
     new Rideshare('Looking For', 'Brampton', 'Montreal', '16 July 2023', 40, 'abcd_1234'),
     new Rideshare('Looking For', 'Missussauga', 'Oshawa', '18 July 2023', 60, 'abcd_1234'),
-    new Rideshare('Offering', 'Hamilton', 'Ottawa', '20 July 2023', 30, 'abcd_1234')
+    new Rideshare('Offering', 'Hamilton', 'Ottawa', '20 July 2023', 30, 'abcd_1234'),
+    new Rideshare('Looking For', 'Niagara Falls', 'Ottawa', '20 July 2023', 30, 'abcd_1234'),
+    new Rideshare('Looking For', 'Niagara Falls', 'Ottawa', '20 July 2023', 30, 'abcd_1234'),
+    new Rideshare('Looking For', 'Niagara Falls', 'Ottawa', '20 July 2023', 30, 'abcd_1234')
   ];
 
   constructor() {}
